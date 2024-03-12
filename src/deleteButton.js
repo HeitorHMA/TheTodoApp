dccc17860a532431b40ff8d55b905c2da2b5d2a3
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-03-12 15:26:50 -0300

    Create delete div function

--- src/deleteButton.js
+++ src/deleteButton.js
@@ -1,8 +1,8 @@
-function deleteButton(button){
+function deleteButton(){
     const deleteButton = document.querySelectorAll(".delete-button");
     deleteButton.forEach(function(button){
-        button.addEventListener('click',function(){
-            const todosContainer= document.querySelector("#todoscontainer")
+        button.addEventListener("click",function(){
+            const todosContainer= document.querySelector("#todosContainer")
             const todoParent = button.parentElement;
             todosContainer.removeChild(todoParent);
 })})}
