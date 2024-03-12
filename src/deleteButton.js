018870e14ba6b1799fd5072248680969da46540c
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-03-12 15:48:31 -0300

    Create delete local storage element function

--- src/deleteButton.js
+++ src/deleteButton.js
@@ -5,5 +5,11 @@ function deleteButton(){
             const todosContainer= document.querySelector("#todosContainer")
             const todoParent = button.parentElement;
             todosContainer.removeChild(todoParent);
+            
+            const index = todosArray.indexOf(todoParent)
+            const todosArray = localStorage.getItem('data',JSON.parse(todosArray));
+            todosArray.splice(index,1);
+            localStorage.setItem('data', JSON.stringify(todosArray));
+            console.log(todosArray.indexOf(todoParent))
 })})}
 export default deleteButton;
--- src/displayTodos.js
+++ src/displayTodos.js
@@ -6,7 +6,12 @@ function displayTodos(){
         todoDiv.classList.add("todos");
         todoDiv.innerHTML = `<input type = "checkbox" class="chckBox" /><h2><span class="usrTsk">${todosArray[i]}</span></h2><button class="delete-button">X</button>`;
         todosContainer.appendChild(todoDiv);
-    }
-}
+
+        const deleteButton = document.querySelectorAll(".delete-button");
+        deleteButton.forEach(function(button){
+            button.addEventListener("click",function(){
+                todosArray.splice(i,1);
+                localStorage.setItem('data', JSON.stringify(todosArray));
+    })})}}
 
 export default displayTodos
